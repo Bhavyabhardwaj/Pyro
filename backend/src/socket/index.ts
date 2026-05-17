9b26ffe6abc03e72f89a9cff54b43fc0b98a70d8
Bhavya Bhardwaj <bbhardwaj303@gmail.com>
2026-05-17 22:55:50 +0530

    feat: add authentication middleware for socket connections and define AuthenticatedSocket type

--- backend/src/socket/index.ts
+++ backend/src/socket/index.ts
@@ -1,6 +1,8 @@
 import { Server } from "socket.io";
+import { AuthenticatedSocket } from "./types";
 
 import type { Server as HTTPServer } from "http";
+import { tokenUtils } from "../utils";
 
 export const initializeSocket = (server: HTTPServer) => {
     const io = new Server(server, {
@@ -9,7 +11,25 @@ export const initializeSocket = (server: HTTPServer) => {
         },
     });
 
-    io.on("connection", (socket) => {
+    io.use((socket: AuthenticatedSocket, next) => {
+        try {
+            const token = socket.handshake.auth.token;
+            if (!token) {
+                return next(new Error("Unauthorized"));
+            }
+            const decoded = tokenUtils.verifyToken(token);
+            if (!decoded || typeof decoded === "string") {
+                return next(new Error("Unauthorized"));
+            }
+            socket.user = {
+                userId: decoded.userId,
+            }
+            next();
+        } catch (error) {
+            next(new Error("Unauthorized"));
+        }
+    });
+    io.on("connection", (socket: AuthenticatedSocket) => {
         console.log(`User connected: ${socket.id}`);
 
         socket.on("disconnect", () => {
