59f0fd961788c7f2fe518124a3aff7ed0e89b4d9
Bhavya Bhardwaj <bbhardwaj303@gmail.com>
2026-05-27 14:13:47 +0530

    feat: update CORS options to match all routes for preflight requests

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -31,7 +31,7 @@ const corsOptions: cors.CorsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options("(.*)", cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
